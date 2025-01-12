a05876ebc30aa131a9df73b915b151510820d10b
CapedBaldy <capedbaldy141@gmail.com>
2025-01-12 01:00:26 +0000

    chore: bump version to 0.0.2-rc.1 in package.json and update useState function to accept a function as value

--- src/hooks/use-state.ts
+++ src/hooks/use-state.ts
@@ -1,23 +1,25 @@
 import { useHookState } from "../topo";
 
-
 interface Storage<T extends unknown> {
-    value?: [T];
+	value?: [T];
 }
 
-export function useState<T extends unknown>(value: T, discriminator?: unknown) {
-    const storage = useHookState<Storage<T>>(discriminator);
+export function useState<T extends unknown>(value: T | (() => T), discriminator?: unknown) {
+	const storage = useHookState<Storage<T>>(discriminator);
 
-    if (storage.value === undefined) {
-        storage.value = [value];
-    }
+	if (storage.value === undefined) {
+		if (typeIs(value, "function")) {
+			storage.value = [value()];
+		} else {
+			storage.value = [value];
+		}
+	}
 
-    const setter = (newValue: T | ((oldValue: T) => T)) => {
-        const value = storage.value![0];
-        if (typeIs(newValue, "function"))
-            newValue = newValue(value);
-        storage.value = [newValue];
-    }
+	const setter = (newValue: T | ((oldValue: T) => T)) => {
+		const value = storage.value![0];
+		if (typeIs(newValue, "function")) newValue = newValue(value);
+		storage.value = [newValue];
+	};
 
-    return $tuple(storage.value[0], setter);
+	return $tuple(storage.value[0], setter);
 }
